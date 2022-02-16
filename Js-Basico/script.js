91a7e2054eb67c784ff18bd249de6c87dad5956e
moacir lopes <mjuniorlopes.10@outlook.com>
2022-02-15 22:47:39 -0300

    10

---
+++ Js-Basico/script.js
@@ -0,0 +1,58 @@
+//
+
+//alert('Hello Word')
+
+//
+
+console.log('Ola mundo')
+
+//Variaveis
+
+//Armazena um dado
+//let é uma variavel que pode altera seu valor
+//const é uma variavel que não pode alterar o seu valor
+// let a = 'Placa de Captura'
+
+// a = 'Hd'
+
+// a = 'Placa de captura'
+
+// console.log(a)
+
+// const ab = 'neymar'
+
+// console.log(ab)
+//erro
+
+//Tipos de Dados
+
+//Boolean -> valor verdadeiro ou falso ; true ou false
+//Null -> nulo
+//Undefined -> indefinido
+//Number -> numeros
+//String -> cadeia de caracteres
+
+let teste = false
+
+console.log(teste)
+console.log(typeof teste)
+
+//Operadores Aritimetricos
+
+// + Adição
+// - Substração
+// * Multiplicação
+// / Divisão
+// % Modulo ; Resto da divisão.
+// ++ Incremento
+// -- Decremento
+
+//exemplos
+
+//Adição
+let a = 10
+a--
+let b = 15
+
+let c = a % b
+console.log(a)
